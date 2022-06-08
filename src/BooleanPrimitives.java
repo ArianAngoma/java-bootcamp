058680143a4aa8165c4e08590f53ac3dfc66ffec
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-06-07 23:50:25 -0500

    Boolean primitives

--- src/BooleanPrimitives.java
+++ src/BooleanPrimitives.java
@@ -1,2 +1,18 @@
 public class BooleanPrimitives {
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        System.out.println("isTrue = " + isTrue);
+
+        boolean isFalse = Boolean.FALSE;
+        System.out.println("isFalse = " + isFalse);
+
+        double doubleNumber = 123456.78E-3;
+        float floatNumber = 124578E3F;
+
+        isTrue = doubleNumber > floatNumber;
+        System.out.println("isTrue = " + isTrue);
+
+        boolean isEqual = doubleNumber == floatNumber;
+        System.out.println("isEqual = " + isEqual);
+    }
 }

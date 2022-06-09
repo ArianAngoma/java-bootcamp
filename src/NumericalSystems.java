7a939f5c9a399e065d1887ba03899c92ca9e14d8
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-06-08 23:33:05 -0500

    Decimal, binary, octal and hexadecimal numbers

---
+++ src/NumericalSystems.java
@@ -0,0 +1,11 @@
+public class NumericalSystems {
+    public static void main(String[] args) {
+        int decimalNumber = 123;
+        System.out.println("decimalNumber = " + decimalNumber);
+
+        System.out.println("binary number = " + Integer.toBinaryString(decimalNumber));
+        
+        int binaryNumber = 0b1111011;
+        System.out.println("binaryNumber = " + binaryNumber);
+    }
+}
